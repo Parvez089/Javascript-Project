8f4459f1e46fe162476fafd5247a12a2673829e4
parvez <mhparvez089@gmail.com>
2025-09-15 23:59:25 +0600

    movie explorer update

--- MovieExplorer/JavaScript/script.js
+++ MovieExplorer/JavaScript/script.js
@@ -1,45 +1,77 @@
-const search = document.getElementById('search')
-const cardList = document.getElementById("movie-card")
 
+const searchInput = document.getElementById("search-input")
+const searchBtn = document.getElementById("searchBtn")
+document.getElementById("searchBtn").addEventListener("keyup", searchProductBtn)
 
-// const url = "https://www.omdbapi.com/?i=avengers&apikey=e18eb460"
-// fetch(url)
-//     .then((res) => res.json())
-//     .then((data) => {
-//         console.log(data.Search)
-//         let movies = data;
+const movieCard = document.getElementById("movie-card");
+const ul = document.createElement("ul")
 
-//         movies.map((movie) => {
-//             let poster = document.createElement("img")
-//             poster.src = data.Poster
-
-//             let title = document.createElement("h2")
-//             title.innerText = data.Title
-//         })
+let productItems = []
 
 
 
-
-//         cardList.appendChild(poster)
-
-//     })
+function displayProducts() {
 
 
-fetch('https://www.omdbapi.com/?s=thor&page&apikey=e18eb460')
-    .then(res => res.json())
-    .then(data => {
-        let movies = data.Search;
-        movies.map(function (movie) {
-            console.log(movie)
-            let img = `<img src=${movie.Poster}>`
-            let h1 = `<h1>${movie.Title}</h1>`
-            console.log(h1)
+    fetch("https://dummyjson.com/products?limit=50&skip=0")
+        .then((res) => res.json())
+        .then((data) => {
+            let products = data.products
 
-            let poster = document.getElementById("#img")
-            img.innerHTML = poster
-            let title = document.getElementById("#title")
-            h1.innerHTML = title
+            products.map(product => {
+            // console.log(product)
+
+                let li = document.createElement('li')
+                let title = document.createElement("h2")
+                let img = document.createElement("img")
+                let price = document.createElement("p")
+                let button = document.createElement("button")
+
+                // console.log(title, img, price)
+
+
+                img.src = product.thumbnail
+                title.innerHTML = product.title
+                price.innerHTML = "Price: " + " " + "$" + product.price
+                button.innerHTML = "Buy Now"
+
+                // movieCard.appendChild(title)
+
+                li.appendChild(img);
+                li.appendChild(title);
+                li.appendChild(price);
+                li.appendChild(button)
+                ul.appendChild(li)
+                // console.log(ul)
+
         })
     })
 
-// 
+    movieCard.appendChild(ul)
+
+}
+displayProducts()
+
+function searchProductBtn() {
+
+    const search = document.getElementById("search-input").value.toLowerCase();
+    console.log(search)
+
+    const filteredValue = productItems.filter((item) =>
+
+        item.title.toLowerCase().includes(search) || item.title.toLowerCase().includes(search)
+    )
+    displayProducts(filteredValue)
+}
+
+// function searchBookBtn() {
+
+//     // const search = document.getElementById("search").value.trim();
+//     const query = document.getElementById("search").value.toLowerCase();
+//     console.log(query)
+//     const filtered = books.filter(book =>
+//         book.title.toLowerCase().includes(query) || book.author.toLowerCase().includes(query));
+//     console.log(filtered)
+
+//     displayBooks(filtered);
+// }
